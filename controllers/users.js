e0e1710407c33b6796a1c6dc7a70158a489b24af
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-03-08 11:21:38 +0300

    edit user

--- controllers/users.js
+++ controllers/users.js
@@ -73,7 +73,53 @@ const CreateUser = async (req, res) => {
     user,
   });
 };
+const EditUser = async (req, res) => {
+  if (!req.isAuth) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  if (!req.user.role === "super_admin") {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  const { id } = req.params;
+  const { name, email, password, role, active } = req.body;
+  if (!name || !email || !role || !active) {
+    return res.status(400).json({
+      message: "Please fill all the fields",
+    });
+  }
+
+  const userExists = await knex("users").where("email", email).first();
+  if (!userExists) {
+    return res.status(400).json({
+      message: "User does not exists",
+    });
+  }
+  let isMatch = false;
+  let salt = null;
+  let hashedPassword = null;
+  if (password) {
+    isMatch = await bcrypt.compare(password, userExists.password);
+    if (!isMatch) {
+      salt = await bcrypt.genSalt(12);
+      hashedPassword = await bcrypt.hash(password, salt);
+    }
+  }
+  const user = await knex("users")
+    .where("id", id)
+    .update({
+      name,
+      email,
+      role,
+      password: isMatch ? userExists.password : hashedPassword,
+      active,
+    });
+  return res.status(200).json({
+    message: "User updated successfully",
+    user,
+  });
+};
 
 exports.AllUsers = AllUsers;
 exports.DeleteUser = DeleteUser;
 exports.CreateUser = CreateUser;
+exports.EditUser = EditUser;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const verifyJWT = require("../middleware/verifyJWT");
-const { CreateUser, AllUsers, DeleteUser } = require("../controllers/users");
+const {
+  CreateUser,
+  AllUsers,
+  DeleteUser,
+  EditUser,
+} = require("../controllers/users");
 router.use(verifyJWT);
 router.get("/all", AllUsers);
 router.post("/create", CreateUser);
+router.patch("/edit/:id", EditUser);
 router.delete("/delete/:id", DeleteUser);
 
 module.exports = router;
