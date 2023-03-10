8ab2f2654e0d3fb64a96780bf846ce08033f10d6
Didar Abdulkhaliq <didar0xalil@gmail.com>
2023-03-10 21:28:40 +0300

    replace jimp by sharp ( less cpu usage )

--- controllers/article.js
+++ controllers/article.js
@@ -1,7 +1,7 @@
 const knex = require("../utils/knex");
 const fs = require("fs");
 const { encode } = require("blurhash");
-const Jimp = require("jimp");
+const sharp = require("sharp");
 const logger = require("../utils/Logger");
 
 const articles = async (req, res) => {
@@ -24,17 +24,12 @@ const createArticle = async (req, res) => {
   if (req.user.role === "user") {
     return res.status(401).json({ message: "Unauthorized" });
   }
-  /// create blurhash
   let blurhash = "";
   if (req.file) {
-    const image = await Jimp.read(req.file.path);
-    const width = image.bitmap.width;
-    const height = image.bitmap.height;
-    const pixels = image.bitmap.data;
-    const blurhashe = encode(pixels, width, height, 4, 4);
-    blurhash = blurhashe;
+    await encodeImageToBlurhash(req.file.path).then((hash) => {
+      blurhash = hash;
+    });
   }
-
   const formData = req.body;
   try {
     await knex("articles").insert({
@@ -104,6 +99,7 @@ const editArticle = async (req, res) => {
   if (!article) {
     return res.status(404).json({ message: "Article not found" });
   }
+  let blurhash = "";
   if (req.file) {
     if (article.image !== "#") {
       fs.unlink(article.image, (err) => {
@@ -117,6 +113,11 @@ const editArticle = async (req, res) => {
         console.log(err);
       }
     );
+    await encodeImageToBlurhash(req.file.path).then((hash) => {
+      blurhash = hash;
+    });
+  } else {
+    blurhash = article.image_blurhash;
   }
   await knex("articles")
     .where("id", req.params.id)
@@ -127,6 +128,7 @@ const editArticle = async (req, res) => {
         ? `uploads/images/${formData.title}.` +
           req.file.originalname.split(".")[1]
         : article.image,
+      image_blurhash: req.file ? blurhash : article.image_blurhash,
     });
   await logger(`Edited article ${article.title}`, req.user.id);
   return res.status(200).json({ message: "Article updated successfully" });
@@ -136,3 +138,14 @@ exports.editArticle = editArticle;
 exports.deleteArticle = deleteArticle;
 exports.createArticle = createArticle;
 exports.articles = articles;
+const encodeImageToBlurhash = (path) =>
+  new Promise((resolve, reject) => {
+    sharp(path)
+      .raw()
+      .ensureAlpha()
+      .resize(64, 64, { fit: "inside" })
+      .toBuffer((err, buffer, { width, height }) => {
+        if (err) return reject(err);
+        resolve(encode(new Uint8ClampedArray(buffer), width, height, 4, 4));
+      });
+  });
